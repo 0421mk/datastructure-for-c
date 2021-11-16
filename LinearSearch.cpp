0f959a6d9a348e3d719cff565aa2ba0d43989521
youngbamer <0421mk@naver.com>
2021-11-16 18:28:25 +0900

    순차 탐색 알고리즘 구현@

--- LinearSearch.cpp
+++ LinearSearch.cpp
@@ -1,31 +1,28 @@
 #include <stdio.h>
 
-int LSearch(int *ar, int len, int target)
-{
-	int i;
-	for (i=0; i<len; i++) {
-		if(ar[i] == target)
+// ADT : 순차탐색
+// LSearch => 탐색 성공시 탐색한 값 반환, 실패시 -1 반환 
+// int 배열 자료형으로 예시 수행 
+
+int LSearch(int * ar, int len, int idx) {
+	for(int i = 0; i < len; i++) {
+		if(ar[i] == idx) {
 			return i;
+		}
 	}
 	return -1;
-}
+} 
 
-int main(void)
-{
-	int arr[] = {3, 5, 2, 4, 9};
-	int idx;
+int main(void) {
 	
-	idx = LSearch(arr, sizeof(arr)/sizeof(int), 4);
-	if(idx == -1)
-		printf("탐색 실패\n");
-	else
-		printf("타겟 저장 인덱스 : %d\n", idx);
+	int ar[] = {2,4,6,8,10};
 	
-	idx = LSearch(arr, sizeof(arr)/sizeof(int), 7);
-	if(idx == -1)
-		printf("탐색 실패\n");
-	else
-		printf("타겟 저장 인덱스 : %d\n", idx);
-		
+	int result = LSearch(ar, sizeof(ar)/sizeof(int), 7);
+	if(result == -1) {
+		printf("존재하지 않습니다.\n");
+	} else {
+		printf("탐색 완료 인덱스 : %d\n", result);
+	}
+	
 	return 0;
 }
