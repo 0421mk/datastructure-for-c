ea0d876ebf8df5e82221107281e170883eb5391e
youngbamer <0421mk@naver.com>
2021-11-21 23:45:01 +0900

    이진 트리 구현

---
+++ Tree/BinaryTree.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "BinaryTree.h"
+
+BTreeNode * MakeBTreeNode(void) // 트리를 만드는 함수 
+{
+	BTreeNode * nd = (BTreeNode*)malloc(sizeof(BTreeNode));
+	nd->left = NULL;
+	nd->right = NULL;
+	
+	return nd;
+}
+
+BTData GetData(BTreeNode * bt) // 트리의 데이터를 Get하는 함수
+{
+	return bt->data;
+}
+
+void SetData(BTreeNode * bt, BTData data) // 트리의 데이터를 Set하는 함수
+{
+	bt->data = data;
+}
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt) // leftSubTree 주소값을 반환
+{
+	return bt->left;
+}
+ 
+BTreeNode * GetRightSubTree(BTreeNode * bt) // rightSubTree 주소값을 반환
+{
+	return bt->right;
+}
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub) // leftSubTree를 지정하는 함수
+{
+	if(main->left != NULL) // leftSubTree가 존재한다면 
+		free(main->left);
+		
+	main->left = sub; // 지정할 주소 값 세팅 
+}
+
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub) // rightSubTree를 지정하는 함수
+{
+	if(main->right != NULL) // rightSubTree가 존재한다면 
+		free(main->right);
+		
+	main->right = sub; // 지정할 주소 값 세팅 
+}
